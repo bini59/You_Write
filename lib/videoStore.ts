9cad6ea7fce2f77d5a73aa6941f953ce1106d49a
bini59 <myuni0234@gmail.com>
2022-11-16 13:55:25 +0900

    feat : clear video
    clear video list

--- lib/videoStore.ts
+++ lib/videoStore.ts
@@ -15,9 +15,11 @@ type video = {
 interface VideoStore {
     video: video[],
     setVideo: (video: video) => void,
+    clearVideo: () => void,
 }
 
 export const useVideoStore = create<VideoStore>((set) => ({
     video: [],
     setVideo: (video) => set((state) => ({ video: [...state.video, video] })),
+    clearVideo: () => set(() => ({ video: [] })),
 }));
