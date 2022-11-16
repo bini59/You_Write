9e14dbdbb34782e15e98a7176cfcfadb199b33fd
bini59 <myuni0234@gmail.com>
2022-11-16 12:23:23 +0900

    feat : add zustand Store
    save video data in store

---
+++ lib/videoStore.ts
@@ -0,0 +1,23 @@
+import create from "zustand";
+
+type video = {
+    id: string,
+    title: string,
+    description: string,
+    thumbnail: {
+        url: string,
+        width: number,
+        height: number,
+    },
+    channelId : string
+}
+
+interface VideoStore {
+    video: video[],
+    setVideo: (video: video) => void,
+}
+
+export const useVideoStore = create<VideoStore>((set) => ({
+    video: [],
+    setVideo: (video) => set((state) => ({ video: [...state.video, video] })),
+}));
